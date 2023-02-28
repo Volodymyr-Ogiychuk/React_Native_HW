048c32ab394ae37c5ffc6429e31cf95c8dd31d25
Volodymyr-Ogiychuk <volodymyr.ogiychuk@gmail.com>
2023-02-28 22:33:42 +0200

    new configuration

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './Screens/LoginScreen';
+import RegistrationScreen from './Screens/RegistrationScreen';
 
 export default function App() {
   return (
@@ -7,6 +9,7 @@ export default function App() {
       <Text>Open up App.js to start working on your app!</Text>
       <Text>HELLO!!!! Glad to see you here!!!</Text>
       <Text>This is your first mob APP!!!</Text>
+      <LoginScreen/>
       <StatusBar style="auto" />
     </View>
   );
